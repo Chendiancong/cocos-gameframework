8c1c7931aed56661cdeefb1a2631376eba365ddd
LAPTOP-BNE3UK1S\Dianc <Diancongchen@outlook.com>
2024-10-26 15:48:58 +0800

    initial

---
+++ project-init.bat
@@ -0,0 +1,5 @@
+call npm install --registry=http://registry.npmmirror.com
+call npm install file:local_node_libs/fairygui-cc
+REM cd /d .\extensions\f1-builder
+REM call npm install --registry=http://registry.npmmirror.com
+PAUSE
